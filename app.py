00fa1eafdb1091d86780bdf35d7ae0fe88e0fe9b
yujeong2 <chyj971008@gmail.com>
2019-11-21 13:14:42 +0900

    first heroku deploy

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 @app.route('/')
 def hello_world():
