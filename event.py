a0511101e6397f75014bca836585b99f9b0ff1e7
yujeong2 <chyj971008@gmail.com>
2019-11-22 23:31:19 +0900

    add temporal response

--- event.py
+++ event.py
@@ -3,6 +3,14 @@ import json
 
 app = Flask(__name__)
 
+response = {
+    "version": "2.0",
+    "resultCode": "OK",
+    "output": {
+        "title": "123"
+    }
+}
+
 @app.route('/eventList', methods=['GET', 'POST'])
 def parse_request():
     data = request.get_json(silent=True, cache=False)
@@ -12,7 +20,8 @@ def parse_request():
         thejson = "no json"
 
     print(thejson)
-    return "good job"
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
