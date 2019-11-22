7833086d8ade98f665044e9df14f4fbf1535e7c5
yujeong2 <chyj971008@gmail.com>
2019-11-22 15:59:42 +0900

    modify getting json

--- event.py
+++ event.py
@@ -3,12 +3,16 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/eventList', methods=['POST'])
-def JsonHandler():
-    print (request.is_json)
-    content = request.get_json()
-    print(content)
-    return 'JSON Posted'
+@app.route('/eventList', methods=['GET', 'POST'])
+def parse_request():
+    data = request.get_json(silent=True, cache=False)
+    if data:
+        thejson = json.dumps(data)
+    else:
+        thejson = "no json"
+
+    print(thejson)
+    return "good job"
 
 if __name__ == '__main__':
     app.run(debug=True)
