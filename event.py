138ff01825faf441b2180f5ca6757c966a7d5d1f
yujeong2 <chyj971008@gmail.com>
2019-11-25 17:45:21 +0900

    parameter parsing

--- event.py
+++ event.py
@@ -14,14 +14,14 @@ response = {
 @app.route('/eventList', methods=['GET', 'POST'])
 def parse_request():
     data = request.get_json(silent=True, cache=False)
+
     if data:
-        thejson = json.dumps(data)
-        print(thejson['action']['parameters'])
+        params = data['action']['parameters']
+        print(jsonify(params))
     else:
         print("no json")
 
     return jsonify(response)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
