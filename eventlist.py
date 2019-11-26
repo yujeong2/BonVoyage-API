15d2c5ee24dc9ab154a7c5ac370df81829246fd7
yujeong2 <chyj971008@gmail.com>
2019-11-27 01:27:46 +0900

    created makedate function

--- eventlist.py
+++ eventlist.py
@@ -1,9 +1,17 @@
 from flask_restful import Api, Resource
 from flask import Flask, jsonify, request
+import datetime
+
 
 app = Flask(__name__)
 api = Api(app)
 
+
+def makedate(ymonth, mday):
+    now = datetime.datetime.now()
+    year = now.strftime('%Y')
+    date = year + ymonth + mday
+    return date
 
 class GetParams(Resource):
 
@@ -11,10 +19,12 @@ class GetParams(Resource):
         data = request.get_json()
 
         location = data['action']['parameters']['location']['value']
-        week = data['action']['parameters']['week']['value']
-        day = data['action']['parameters']['day']['value']
+        ymonth = data['action']['parameters']['ymonth']['value']
+        mday = data['action']['parameters']['mday']['value']
 
-        print({'location': location, 'week': week, 'day': day})
+        date = makedate(ymonth, mday)
+
+        print({'location': location, 'ymonth': ymonth, 'mday': mday, 'date': date})
 
         response = {
             "version": "2.0",
